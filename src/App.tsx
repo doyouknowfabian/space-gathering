a23a608975edfb8a5921fd11452afc0157130fb9
doyouknowfabian <123430185+doyouknowfabian@users.noreply.github.com>
2024-08-13 01:30:50 -0400

    Add game board and sprites (#3)
    * added 4 colored triangle sprites
    
    * created gameboard component
    
    * added gameboard with 4 sprites to appear on screen
    
    ---------
    
    Co-authored-by: FabianV <fabianv@mit.edu>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { redTri, greenTri, blueTri, yellowTri } from './sprites'
+import { Gameboard } from './components';
 import './App.css';
 
 
@@ -9,13 +11,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>SPACE<br></br>GATHERING</h1>
-        
-        
         <div>
           <label htmlFor="playerName">Enter Your Name Below</label><br></br>
           <input id="playerName" placeholder={playerName}></input><br></br><br></br>
           <a href="#" className="playButton">PLAY</a>
         </div>
+        <Gameboard>
+          <img src={redTri} width="50px" height="50px"/>
+          <img src={greenTri} width="50px" height="50px"/>
+          <img src={blueTri} width="50px" height="50px"/>
+          <img src={yellowTri} width="50px" height="50px"/>
+        </Gameboard>
       </header>
     </div>
   );
---
+++ src/sprites/index.ts
@@ -0,0 +1,6 @@
+import redTri from './red-triangle.png';
+import greenTri from './green-triangle.png';
+import blueTri from './blue-triangle.png';
+import yellowTri from './yellow-triangle.png';
+
+export { redTri, greenTri, blueTri, yellowTri };
